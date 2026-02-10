5d648d134a023188691b111f5ca2cd39f4468ea1
Shesh-21 <akshadashesh02@gmail.com>
2026-02-10 19:19:54 +0530

    Syllabus done

---
+++ frontend/src/components/Syllabus/CommonExamTables.jsx
@@ -0,0 +1,133 @@
+export default function CommonExamTables() {
+  return (
+    <>
+      {/* ===== TABLE 2 ===== */}
+      <h4 className="table-sub-heading">
+        2nd Term Non Academic & Practical Exam Schedule (2025–26)
+      </h4>
+
+      <div className="table-wrapper">
+        <table className="syllabus-table syllabus-table-center">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Class I</th>
+              <th>Class II</th>
+              <th>Class III</th>
+              <th>Class IV</th>
+              <th>Class V</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>27/01/26</td>
+              <td>English / Hindi (Recitation)</td>
+              <td>English / Hindi (Recitation)</td>
+              <td>English / Hindi (Reading comprehension)</td>
+              <td>English / Hindi (Reading comprehension)</td>
+              <td>English / Hindi (Reading comprehension)</td>
+            </tr>
+            <tr>
+              <td>28/01/26</td>
+              <td>Maths / EVS (Activity)</td>
+              <td>Maths / EVS (Activity)</td>
+              <td>Maths / EVS (Activity)</td>
+              <td>Maths / EVS (Activity)</td>
+              <td>Maths / Science (Activity)</td>
+            </tr>
+            <tr>
+              <td>29/01/26</td>
+              <td>G.K (MCQ) & Drawing (Art & Craft)</td>
+              <td>G.K (MCQ) & Drawing (Art & Craft)</td>
+              <td>G.K (MCQ) & Drawing (Art & Craft)</td>
+              <td>G.K (MCQ) & Drawing (Art & Craft)</td>
+              <td>G.K (MCQ) & Drawing (Art & Craft) & S.St (Activity)</td>
+            </tr>
+            <tr>
+              <td>31/01/26</td>
+              <td>Computer (MCQ)</td>
+              <td>Computer (MCQ)</td>
+              <td>Computer (MCQ)</td>
+              <td>Computer (MCQ)</td>
+              <td>Computer (MCQ)</td>
+            </tr>
+            <tr>
+              <td>02/02/26</td>
+              <td>Abacus</td>
+              <td>Abacus</td>
+              <td>Abacus</td>
+              <td>Abacus</td>
+              <td>Abacus</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      {/* ===== TABLE 3 ===== */}
+      <h4 className="table-sub-heading">
+        2nd Term Examination (Annual Exam) (2025–26)
+      </h4>
+
+      <div className="table-wrapper">
+        <table className="syllabus-table syllabus-table-center">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Class I</th>
+              <th>Class II</th>
+              <th>Class III</th>
+              <th>Class IV</th>
+              <th>Class V</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>16/02/2026</td>
+              <td>Hindi</td>
+              <td>Hindi</td>
+              <td>English</td>
+              <td>Maths</td>
+              <td>Maths</td>
+            </tr>
+            <tr>
+              <td>18/02/2026</td>
+              <td>Maths</td>
+              <td>EVS</td>
+              <td>Hindi</td>
+              <td>English</td>
+              <td>Science</td>
+            </tr>
+            <tr>
+              <td>20/02/2026</td>
+              <td>English</td>
+              <td>English</td>
+              <td>Maths</td>
+              <td>EVS</td>
+              <td>Hindi</td>
+            </tr>
+            <tr>
+              <td>23/02/2026</td>
+              <td>EVS</td>
+              <td>Maths</td>
+              <td>EVS</td>
+              <td>Hindi</td>
+              <td>S.St</td>
+            </tr>
+            <tr>
+              <td>25/02/2026</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>English</td>
+            </tr>
+            <tr>
+              <td>27/02/2026</td>
+              <td colSpan="5"><b>Reserve Date</b></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
